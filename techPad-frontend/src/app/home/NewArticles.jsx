9b8e8fe51ab59951d57bf6ffb85f8ec797f4c5ca
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-10-15 11:40:46 +0900

    新着記事エリアの作成

---
+++ techPad-frontend/src/app/home/NewArticles.jsx
@@ -0,0 +1,36 @@
+import { Box, Typography } from "@mui/material";
+import ArticleCard from "../../components/Article/Card";
+
+const articles = [
+    {id: 1, title: 'タイトル', user: 'ユーザー', date: '2024-01-01', text: '本文', favorite: 1, comment: 1},
+    {id: 2, title: 'タイトル', user: 'ユーザー', date: '2024-01-02', text: '本文', favorite: 2, comment: 2},
+    {id: 3, title: 'タイトル', user: 'ユーザー', date: '2024-01-03', text: '本文', favorite: 3, comment: 3}
+];
+
+export default function NewArticles() {
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem', padding: '1rem 2.5rem', }}>
+            <Typography
+                sx={{
+                    fontSize: { xs: '1.5rem', md: '2.5rem' },
+                    fontWeight: 'bold',
+                    color: 'navy.main',
+                    margin: { xs: '.5rem 0', md: '1.5rem 0'}
+                }}
+            >
+                New topics
+            </Typography>
+            {articles.map((article) => (
+                <ArticleCard
+                    key={article.id}
+                    title={article.title}
+                    user={article.user}
+                    date={article.date}
+                    text={article.text}
+                    favorite={article.favorite}
+                    comment={article.comment}
+                />
+            ))}
+        </Box>
+    );
+}
