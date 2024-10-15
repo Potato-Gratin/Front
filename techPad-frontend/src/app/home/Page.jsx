006201cd7753f1c72d22f1e991cd052a53fe8591
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-10-15 12:15:55 +0900

    注目記事エリアの追加

--- techPad-frontend/src/app/home/Page.jsx
+++ techPad-frontend/src/app/home/Page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../../components/HeroSection";
 import NewArticles from "./NewArticles";
+import TrendArticles from "./TrendArticles";
 
 export default function Home() {
     return (
         <>
             <HeroSection />
             <NewArticles />
+            <TrendArticles />
         </>
     );
 }
