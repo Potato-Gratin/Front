0f37a7341a0d78d782d5ee24804863937332a992
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-10-15 11:40:59 +0900

    ホームページの作成

---
+++ techPad-frontend/src/app/home/Page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "../../components/HeroSection";
+import NewArticles from "./NewArticles";
+
+export default function Home() {
+    return (
+        <>
+            <HeroSection />
+            <NewArticles />
+        </>
+    );
+}
