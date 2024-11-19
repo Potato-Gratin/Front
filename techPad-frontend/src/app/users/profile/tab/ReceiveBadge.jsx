5a4ed17bc4f88663ec3d6fe71098acab00d3c170
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-11-19 09:54:49 +0900

    プロフィール表示画面_タブ内要素仮置き

---
+++ techPad-frontend/src/app/users/profile/tab/ReceiveBadge.jsx
@@ -0,0 +1,7 @@
+export default function ReceiveBadge() {
+    return (
+        <>
+        <p>受信バッジ一覧</p>
+        </>
+    );
+}
---
+++ techPad-frontend/src/app/users/profile/tab/Review.jsx
@@ -0,0 +1,7 @@
+export default function Review() {
+    return (
+        <>
+        <p>投稿レビュー一覧</p>
+        </>
+    );
+}
