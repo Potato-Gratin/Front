96b1013ba7f06850eb3b9448aa56f542dc0ccf89
ulxsth <ulxsth@gmail.com>
2024-11-26 14:41:53 +0900

    feat: 紐づけるリンクのidをdisplay_idにした

--- techPad-frontend/src/app/searchResult/UserResult.jsx
+++ techPad-frontend/src/app/searchResult/UserResult.jsx
@@ -2,19 +2,34 @@
 
 import { Box, Card, CardContent, Typography, useTheme } from "@mui/material";
 import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
-import { useNavigate } from "react-router-dom";
-
-const users = [
-    { uid: 1, id: '@aaaaa', name: 'user1', profile: 'profile1', articles: 10 },
-    { uid: 2, id: '@bbbbb', name: 'user2', profile: 'profile2', articles: 20 },
-    { uid: 3, id: '@ccccc', name: 'user3', profile: 'profile3', articles: 30 },
-    { uid: 4, id: '@ddddd', name: 'user4', profile: 'profile4', articles: 40 },
-    { uid: 5, id: '@eeeee', name: 'user5', profile: 'profile5', articles: 50 },
-]
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function UserResult() {
     const theme = useTheme();
     const navigate = useNavigate();
+
+    const [users, setUsers] = useState([
+        { id: 1, display_id: 'aaaaa', name: 'user1', description: 'profile1', articles: 10 },
+        { id: 2, display_id: 'bbbbb', name: 'user2', description: 'profile2', articles: 20 },
+        { id: 3, display_id: 'ccccc', name: 'user3', description: 'profile3', articles: 30 },
+        { id: 4, display_id: 'ddddd', name: 'user4', description: 'profile4', articles: 40 },
+        { id: 5, display_id: 'eeeee', name: 'user5', description: 'profile5', articles: 50 },
+    ])
+
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const keyword = query.get('q');
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_ORIGIN + "/users/search?q=" + keyword)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                setUsers(data)
+            })
+            .catch((error) => console.error(error));
+    }, [])
 
     const handleClick = (id) => {
         navigate(`/user?id=${id}`);
@@ -31,9 +46,9 @@ export default function UserResult() {
                     padding: { xs: '0 1rem', md: '0 2.5rem' }
                 }}>
                 {users.map((user) => (
-                    <div key={user.uid}>
+                    <div key={user.display_id}>
                         <Card
-                            onClick={() => { handleClick(user.uid); }}
+                            onClick={() => { handleClick(user.display_id); }}
                             sx={{
                                 display: { xs: 'none', md: 'block' },
                                 width: '50vw',
@@ -42,16 +57,16 @@ export default function UserResult() {
                                 marginBottom: '1rem',
                                 borderRadius: '2rem',
                                 cursor: 'pointer'
-                        }}>
+                            }}>
                             <CardContent>
                                 <div style={{ padding: '0 1rem' }}>
                                     <div style={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
                                         <Typography sx={{ color: `${theme.palette.navy.main}`, fontSize: '1.5rem', fontWeight: 'bold' }}>{user.name}</Typography>
-                                        <Typography sx={{ color: 'gray', fontSize: '.8rem', alignSelf: 'end', padding: '.2rem 0' }}>{user.id}</Typography>
+                                        <Typography sx={{ color: 'gray', fontSize: '.8rem', alignSelf: 'end', padding: '.2rem 0' }}>{user.display_id}</Typography>
                                     </div>
                                     <hr />
                                     <div style={{ margin: '0 1rem' }}>
-                                        <Typography sx={{ color: `${theme.palette.navy.main}`, fontSize: '1rem' }}>{user.profile}</Typography>
+                                        <Typography sx={{ color: `${theme.palette.navy.main}`, fontSize: '1rem' }}>{user.description}</Typography>
                                         <div style={{ marginTop: '1rem', display: 'flex', alignItems: 'center', gap: '.3rem', justifyContent: 'end' }}>
                                             <InsertDriveFileOutlinedIcon sx={{ color: `${theme.palette.navy.main}`, fontSize: '1rem' }} />
                                             <Typography sx={{ color: `${theme.palette.navy.main}`, fontSize: '.9rem' }}>{user.articles}</Typography>
