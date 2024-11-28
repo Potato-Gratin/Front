462ce20fb7984198bf45be0c036e417a79c4f0f0
yamada1023 <ayuka.yamada.tosu@gmail.com>
2024-11-28 11:51:33 +0900

    ルーティング

--- techPad-frontend/src/router/AppRoutes.jsx
+++ techPad-frontend/src/router/AppRoutes.jsx
@@ -3,18 +3,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../app/home/Page';
 import SearchPage from '../app/SearchPage';
-import Result from '../app/searchResult/Page';
 import Auth from '../app/users/auth/Auth';
 import SettingProfile from '../app/users/SettingProfile';
 import AuthRedirect from '../app/users/auth/AuthRedirect';
 import Profile from '../app/users/profile/Page';
 import CreateArticles from '../app/CreateArticles';
 import ArticleDetail from '../app/ArticleDetail';
+import ArticlesResult from '../app/searchResult/ArticlesResult';
+import UserResult from '../app/searchResult/UserResult';
 
 const routes = [
     { path: '/', element: <Home /> },   // ホーム画面
     { path: '/search', element: <SearchPage /> },   // 検索画面
-    { path: '/result', element: <Result /> },   // 検索結果画面
+    { path: '/article/result', element: <ArticlesResult /> },   // 検索結果画面
+    { path: '/user/result', element: <UserResult /> },   // 検索結果画面
     { path: '/auth', element: <Auth /> },   // ログイン画面
     { path: '/auth/redirect', element: <AuthRedirect /> },  // ログイン後のリダイレクト先ページ
     { path: '/userSetting', element: <SettingProfile /> },   // 新規ユーザーの情報登録画面
